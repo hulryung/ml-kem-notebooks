d4ce3946c8c00d7a85e047f0e76fc01d57f0eb3e
Daekeun Kang <dkkang@huconn.com>
2026-04-22 18:57:17 +0900

    chore(quantum): scaffold pqc_edu.quantum subpackage

---
+++ pqc_edu/quantum/__init__.py
@@ -0,0 +1,6 @@
+"""Educational quantum state-vector simulator and Shor's algorithm.
+
+Not for production quantum computing. Scales to ~14 qubits on a laptop.
+All code is intentionally simple so you can trace every step of a quantum
+algorithm without black boxes.
+"""
